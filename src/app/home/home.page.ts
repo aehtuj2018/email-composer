7bb41029455bc9c25670830ac0665feb236f44dd
aehtuj2018 <73031151+aehtuj2018@users.noreply.github.com>
2020-12-03 21:07:14 -0700

    added files

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { EmailComposer } from '@ionic-native/email-composer/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,22 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private emailComposer: EmailComposer) { }
+
+  composeEmail() {
+    this.emailComposer.isAvailable().then((available: boolean) => {
+      if (available) {
+      }
+     });
+
+   const email = {
+    to: 'testto@test.ca',
+    subject: 'Test Email',
+    body: 'Hi This is Test Email Body',
+   
+  };
+  
+  // Send a text message using default options
+this.emailComposer.open(email);
 
 }
